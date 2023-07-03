9553454982cdc9f2f68fee9c38876e07a10d1226
ammarkero <khairulammar.my@gmail.com>
2023-07-04 03:21:27 +0800

    migration: create tables for users, roles, user roles, user logs, external data, posts, and images

--- database/migrations/2023_07_03_172919_create_user_logs_table.php
+++ database/migrations/2023_07_03_172919_create_user_logs_table.php
@@ -11,11 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_logs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->timestamp('login_timestamp');
+            $table->timestamp('logout_timestamp')->nullable();
+            $table->string('token');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -25,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_logs');
     }
 };
--- database/migrations/2023_07_03_172931_create_external_data_table.php
+++ database/migrations/2023_07_03_172931_create_external_data_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('external_data', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->unsignedBigInteger('user_id');
+            $table->json('data')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('external_data');
     }
 };
